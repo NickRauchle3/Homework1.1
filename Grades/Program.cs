719bbc63cf96428bcee8625b60deb4de34c9a16d
nicho <nicho@DESKTOP-B7SKEDU>
2020-02-06 18:25:14 -0600

    Homework 1 SalesTax =  I calculated how to find the sales tax, sales price and etc.. Grades = Figured out how to make a program to determine your final grade.

---
+++ Grades/Program.cs
@@ -0,0 +1,49 @@
+﻿//Nicholas Rauchle
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //clarifying variables
+            string firstName;
+            string lastName;
+            int id;
+            double quizzes;
+            double homeworks;
+            double midterm;
+            double final;
+            double participation;
+
+            //Questions part
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+            Console.WriteLine("What is your student ID?");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for Quizzes?");
+            quizzes = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for Homeworks?");
+            homeworks = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for Participations?");
+            participation = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the Midterm?");
+            midterm = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the Final Exam?");
+            final = Convert.ToDouble(Console.ReadLine());
+
+            //Final Statement
+            double FinalGrade;
+            FinalGrade = (quizzes + homeworks + final + midterm + participation) / 5;
+            Console.WriteLine(firstName + lastName + id + ", your final grade is " + FinalGrade);
+            Console.ReadKey();
+        }
+    }
+}
--- SalesTax/Program.cs
+++ SalesTax/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Nicholas Rauchle
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,34 @@ namespace SalesTax
 {
     class Program
     {
+        const double TaxRate = 8.5;
         static void Main(string[] args)
         {
+            //Stating all required constants
+            string itemName;
+            int myIntegar;
+            double price;
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            itemName = Console.ReadLine();
+            Console.WriteLine("How many " + itemName + " do you want to purchase?");
+            myIntegar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the price of " + itemName + "?");
+            price = Convert.ToDouble(Console.ReadLine());
+
+            //Code for writing the subtotal, sales tax, and the sales total
+            double Subtotal = myIntegar * price;
+            double SalesTax = Subtotal * TaxRate;
+            double SalesTotal = Subtotal + SalesTax;
+
+            Console.WriteLine("Your subtotal is " + Subtotal);
+            Subtotal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your sales tax is " + SalesTax);
+            SalesTax = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your Sales Total is " + SalesTotal);
+            SalesTotal = Convert.ToDouble(Console.ReadLine());
+
+            Console.ReadKey();
+            //End of the code
         }
     }
 }
